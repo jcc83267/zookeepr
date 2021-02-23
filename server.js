7181bcbf66fcbd3d93e42fa1f7617e218d826835
James Canlas <jamescanlasjr@yahoo.com>
2021-02-22 19:05:08 -0800

    changed port to heroku default port or our default of 3001

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
+
+const PORT = process.env.PORT || 3001;
 //used to initialize api server?
 const app = express();
 
@@ -49,6 +51,6 @@ app.get('/api/animals', (req, res) => {
 });
 
 //to listen must be at the end???
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
 });
