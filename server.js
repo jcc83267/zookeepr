771b27e19a17d407b634aee6e8f640b2a43b001e
James Canlas <jamescanlasjr@yahoo.com>
2021-02-22 19:26:32 -0800

    small change to code

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { animals } = require('./data/animals.json');
+
 
 //used to initialize api server?
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const { animals } = require('./data/animals.json');
 //function to filter
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
